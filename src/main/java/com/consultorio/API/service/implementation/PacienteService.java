72f7df2b401b6a897f91c71de05175c487dd189e
Em <emmanuel.lobato9@gmail.com>
2024-03-31 00:11:13 -0300

    Funcionalidades en JS completo - Botones probados con buttons de prueba, ver cuando repo completo con DB y post

--- src/main/java/com/consultorio/API/service/implementation/PacienteService.java
+++ src/main/java/com/consultorio/API/service/implementation/PacienteService.java
@@ -30,7 +30,7 @@ public class PacienteService implements IPacienteService {
     @Override
     public List<Paciente> listarTodos() {
 
-        return null;
+        return pacienteRepository.findAll();
     }
 
     @Override
